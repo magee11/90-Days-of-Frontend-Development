3dc211690bd1cb0a01e08daa0da7df393e7cbeec
Magesh <mageshmarch@gmail.com>
2025-09-22 23:47:30 +0530

    solved counter program

---
+++ React Interview Problems/Counter.jsx
@@ -0,0 +1,16 @@
+// Counter.js
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Counter: {count} </h2>
+      <button onClick={() => setCount((prev)=>prev + 1)}>Increment</button>
+      <button onClick={() => setCount((prev)=>prev - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
